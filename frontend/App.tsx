b29b61848579960ff4ec176bf16457adcd25e4e4
ancez <Lukasz.czapiewski@mmtm.io>
2021-11-08 22:41:27 +0000

    update App.tsx

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,14 +1,15 @@
 import React from "react"
-import { Room } from "./Room"
-import { Home } from "./Home"
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+import { RoomView } from "./RoomView"
+import { HomeView } from "./HomeView"
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/room/:name" component={Room} />
-        <Route path="/" exact component={Home} />
+        <Route path="/room/:name" component={RoomView} />
+        <Route path="/" exact component={HomeView} />
       </Switch>
     </Router>
   )
